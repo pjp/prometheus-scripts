6ec25d6b10a1bfb02f2e4e0f54435b9135ada1c0
pjp <paul@pearceful.net>
2024-02-13 07:43:31 +0000

    Added another script

---
+++ bin/get-latest-speeds-from-prometheus.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+###########################
+# The files to check/create
+#
+SPEED_FILE="${1:-/tmp/speed.txt}"
+SPEED_FILE_TMP="${SPEED_FILE}.from-prometheus"
+
+###################################################################
+# Create the temp. speed file with the latest stats from Prometheus
+#
+source ~/bin/prometheus-lib.sh
+
+dl="$(prometheus-lib-get-latest-value speedtest_download_bits_per_second 1h | awk '{printf "%4.0f\n", $1/1000000}')"
+
+ul="$(prometheus-lib-get-latest-value speedtest_upload_bits_per_second 1h | awk '{printf "%4.0f\n", $1/1000000}')"
+
+echo "speedtest_download $dl" | tee    $SPEED_FILE_TMP
+echo "speedtest_upload   $ul" | tee -a $SPEED_FILE_TMP
+
+#########################################################
+# Have the speed outputs changed since the last check ?
+#
+diff -q $SPEED_FILE $SPEED_FILE_TMP > /dev/null 2>&1
+   
+if [ $? -gt 0 ]
+then
+   #################
+   # Yes, update the speed file
+   #
+   cp $SPEED_FILE_TMP $SPEED_FILE
+fi
